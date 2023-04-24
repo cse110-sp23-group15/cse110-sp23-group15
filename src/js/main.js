65c18a37dbe64814020605575a80bc04b2fe9cc6
akann0 <akann@ucsd.edu>
2023-04-24 10:30:23 -0700

    delay added

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ const magic8Ball = document.querySelector(".magic-8-ball");
 const shakeButton = document.querySelector(".shake-button");
 const textInput = document.getElementById("question");
 import { getRandomResponse } from "./magic8Ball.js";
+import { getRandomInt } from "./utils.js";
+const min_time = 200;
+const max_time = 500;
   
 
 /**
@@ -11,10 +14,13 @@ import { getRandomResponse } from "./magic8Ball.js";
   */
 function shakeMagic8Ball() {
   const response = getRandomResponse();
+  const timeout = getRandomInt(min_time, max_time);
   if (textInput.value=="") {
     alert("Our noodle doesn't know what you want to ask if you don't enter anything");
   } else {
+    setTimeout(function () {
     magic8Ball.textContent = response;
+    }, timeout);
   }
 }
 
