6d41b978331ecdf6dddb8a5b3e3d562636609527
Henri-XYu02 <xinleyu021213@gmail.com>
2023-05-18 21:53:03 -0700

    Edit for JSDoc format

--- fortunetelling/src/__test__/samplemobiletest.js
+++ fortunetelling/src/__test__/samplemobiletest.js
@@ -1,41 +1,38 @@
-import {remote} from 'webdriverio';
-import { expect } from 'expect-webdriverio'
-/*We are simply writing tests based on webdriverio*/
-
+import { remote } from 'webdriverio';
+import { expect } from 'expect-webdriverio';
+/* We are simply writing tests based on webdriverio*/
 
 const capabilities = {
-  platformName: 'Android',
-  'appium:automationName': 'UiAutomator2',
-  'appium:deviceName': 'Android',
-  'browserName': 'Chrome', //You need to have Chrome in simulator or your connected phone
+	platformName: 'Android',
+	'appium:automationName': 'UiAutomator2',
+	'appium:deviceName': 'Android',
+	browserName: 'Chrome' // You need to have Chrome in simulator or your connected phone
 };
 
 const wdOpts = {
-  host: process.env.APPIUM_HOST || 'localhost',
-  port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
-  logLevel: 'info',
-  capabilities,
+	host: process.env.APPIUM_HOST || 'localhost',
+	port: parseInt(process.env.APPIUM_PORT, 10) || 4723,
+	logLevel: 'info',
+	capabilities
 };
 
 /**
-* @param wdOpts: the capabilities and remote opts for webdriverIO to run tests on mobile end
-* @return: none
-*/
-
-async function runTest(wdOpts) {
-  const driver = await remote(wdOpts);
-  //When starting chrome it takes me as a new user, I need to manually click "accept & continue" and choose whether to sync, something to address
-  try {
-    await driver.url('https://www.google.com');
-    await expect(driver).toHaveUrl('https://www.google.com/');
-    
-  } finally {
-    await driver.pause(1000);
-    await driver.deleteSession();
-  }
+ * @param {void}: the capabilities and remote opts for webdriverIO to run tests on mobile end
+ * @return {void}
+ */
+async function runTest() {
+	const driver = await remote(wdOpts);
+	// When starting chrome it takes me as a new user, I need to manually click "accept & continue" and choose whether to sync, something to address
+	try {
+		await driver.url('https://www.google.com');
+		await expect(driver).toHaveUrl('https://www.google.com/');
+	} finally {
+		await driver.pause(1000);
+		await driver.deleteSession();
+	}
 }
 
-runTest(wdOpts).catch(console.error);
+runTest().catch(console.error);
 
 /*
 I tried using jest but it came out with lots of problems
--- fortunetelling/src/__test__/sampleunit.test.js
+++ fortunetelling/src/__test__/sampleunit.test.js
@@ -1,4 +1,4 @@
-/*import {horoscope} from ('fortunetelling\src\js\genHoroscope.js');//need to do module.export={getHoroscope}; in the original file
+/* import {horoscope} from ('fortunetelling\src\js\genHoroscope.js');//need to do module.export={getHoroscope}; in the original file
 
 test('Horoscope Generation'){
     expect().toBe();
