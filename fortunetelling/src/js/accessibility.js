dae632513ce6fb28dcf138ab8e0cb62d964c0998
YueSteveYin <yuy045@ucsd.edu>
2023-05-30 18:28:24 -0700

    accessibility

--- fortunetelling/src/js/accessibility.js
+++ fortunetelling/src/js/accessibility.js
@@ -1,24 +1,49 @@
-function init(){
-    
-}
-
 function accessswitch(){
-    let accessibility = document.getElementsByClassName("accessibility");
+    const synth = window.speechSynthesis;
+    let voiceList = window.speechSynthesis.getVoices();
+    let accessibility = document.getElementsByName("accessibility");
     accessibility[0].addEventListener('change', function(){
+        console.log("change");
         if(this.checked){
+            if(document.URL.includes("index")){
+                let intro = new SpeechSynthesisUtterance("Welcom to main page of tasty noodle fortune telling site");
+                intro.rate = 2;
+                intro.voice = voiceList[0];
+                synth.speak(intro);
+                console.log("intro");
+            }else if(document.URL.includes("questionnaire")){
+                console.log("question");
+            }else if(document.URL.includes("about")){
+                console.log("about");
+            }else if(document.URL.includes("fortune")){
+                console.log("fortune");
+            }else if(document.URL.includes("profiles")){
+                console.log("profiles");
+            }else{
+                let intro = new SpeechSynthesisUtterance("Welcom to main page of tasty noodle fortune telling site");
+                intro.rate = 2;
+                intro.voice = voiceList[0];
+                synth.speak(intro);
+                accessEn()
+                console.log("intro");
+            }
             accessEn();
         } else{
-
+            synth.cancel();
         }
     });
 }
 
 function accessEn(){
-    if(page.url() == "http://127.0.0.1:5500/fortunetelling/index.html"){
-        
-    }
-    let readText = page.getElementsByClassName("read");
+    const synth = window.speechSynthesis;
+    let readText = document.getElementsByClassName("read");
+    let voiceList = window.speechSynthesis.getVoices();
     for (let i = 0; i<readText.length; i++){
-
+        let text =  new SpeechSynthesisUtterance(readText[i].value);
+        text.rate = 2;
+        text.voice = voiceList[0];
+        synth.speak(text);
     }
 }
+
+export {accessswitch, accessEn};
---
+++ fortunetelling/src/js/main.js
@@ -0,0 +1,6 @@
+import { accessswitch} from './accessibility.js'
+window.addEventListener('DOMContentLoaded', init);
+
+function init(){
+    accessswitch();
+}
