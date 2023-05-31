17bbf8952584cbba37d9e65f0bf37603f0ecc031
Henri-XYu02 <xinleyu021213@gmail.com>
2023-05-30 20:35:01 -0700

    test: add primary profile page test

--- fortunetelling/src/__test__/e2e/profile.test.js
+++ fortunetelling/src/__test__/e2e/profile.test.js
@@ -1,5 +1,5 @@
-const timeout = 5000;
-
+const timeout = 20000;
+import { getHoroscope } from '../../js/genHoroscope.js';
 describe('Noodle Profile', () => {
 	beforeAll(async () => {
 		await page.goto(
@@ -8,11 +8,29 @@ describe('Noodle Profile', () => {
 	});
 
 	it('should get to the expected noodle profile', async () => {
-        
+        for (let i = 0;i < 3;++i){
+			const noodleName = await page.$eval('#name h2', (el) => el.innerText);
+			expect(noodleName).toBe(`Noodle ${(i+1)%3}`)
+			const carousel = await page.$('#carousel-image');
+			expect(carousel.alt).toBe(`Noodle ${(i+1)%3}`);
+			await page.click('#next-noodle');
+		}
+		for (let i = 0;i < 3;++i){
+			const noodleName = await page.$eval('#name h2', (el) => el.innerText);
+			expect(noodleName).toBe(`Noodle ${(1-i)%3}`)
+			const carousel = await page.$('#carousel-image');
+			expect(carousel.alt).toBe(`Noodle ${(1-i)%3}`);
+			await page.click('#prev-noodle');
+		}
 	}, timeout);
 
-    it('should review the expected fortune', async () =>{
-
+				
+    it('should see a fortune', async () =>{
+		await page.click('#next-noodle');
+		await page.click('#fortune-button');
+		const fortune = await page.$('#fortuneText').innerText;
+		const fortuneExpected = genHoroscope(2);
+		expect(fortune).toBe()
     }, timeout);
 
 });
