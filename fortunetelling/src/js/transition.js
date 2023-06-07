9f11e06920f2ca8bf46811c3ee15c07771261358
Mark Lucernas <marklcrns@gmail.com>
2023-06-06 19:42:28 -0700

    docs: Add transition docs

--- fortunetelling/src/js/transition.js
+++ fortunetelling/src/js/transition.js
@@ -1,16 +1,20 @@
+/**
+ * Transition Class for page transition
+ */
 class transition extends highway.Transition {
-  // Built-in methods
-  in({ from, to, trigger, done }) {
-    // [...]
-    from.remove();
-    gsap.fromTo(to, 0.5, {opacity:0}, {opacity:1, onComplete: done});
-    console.log("transition active");
-  }
+	/** Fade in function */
+	in({ from, to, done }) {
+		// [...]
+		from.remove();
+		gsap.fromTo(to, 0.5, { opacity: 0 }, { opacity: 1, onComplete: done });
+		console.log('transition active');
+	}
 
-  out({ from, trigger, done }) {
-    // [...]
-    gsap.fromTo(from, 0.5, {opacity:1}, {opacity:0, onComplete: done});
-  }
+	/** Fade out function */
+	out({ from, done }) {
+		// [...]
+		gsap.fromTo(from, 0.5, { opacity: 1 }, { opacity: 0, onComplete: done });
+	}
 }
 
 // Don`t forget to export your transition
