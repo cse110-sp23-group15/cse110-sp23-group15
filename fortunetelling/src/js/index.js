3949b45e0d605831611016038976fbadcfbab7b3
akann0 <akann@ucsd.edu>
2023-06-07 09:46:45 -0700

    fortune button added

--- fortunetelling/src/js/index.js
+++ fortunetelling/src/js/index.js
@@ -4,3 +4,23 @@
 // See examples:
 // - https://jsdoc.app/howto-es2015-modules.html
 // - https://jsdoc.app/howto-es2015-classes.html
+
+window.addEventListener('DOMContentLoaded', init);
+
+/** On load function */
+async function init() {
+    const fortuneLink = document.getElementById('fortuneLink');
+    const fortuneButton = document.getElementById('fortuneButton');
+    const toQuestions = document.getElementById('toQuestions');
+    if (localStorage.getItem('noodleIndex') === null) {
+        fortuneLink.href = '#0';
+        fortuneLink.addEventListener('click', function () {
+            // activate animation for toQuestions button
+            toQuestions.style.animation = "pulse 2s infinite";
+            fortuneLink.textContent = 'You must do the Quiz First!';
+            fortuneLink.style.textDecoration = "none";
+            fortuneLink.style.fontWeight = "bold";
+            fortuneLink.style.color = "#da0b0b";
+        });
+    }
+}
