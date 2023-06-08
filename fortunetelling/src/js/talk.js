064923ab4e9c4144b8d070f0dd37a1450e042877
Runhui-Wang <ruw027@ucsd.edu>
2023-06-08 16:10:33 -0700

    Update Parameter

--- fortunetelling/src/js/talk.js
+++ fortunetelling/src/js/talk.js
@@ -1,149 +1,104 @@
+/**
+ * This script provides an interactive story-telling or conversation experience 
+ * on a website with a noodle shop theme. It triggers upon DOM content load and 
+ * handles user interaction through button clicks, displaying messages, 
+ * animating elements, and changing event listeners dynamically.
+ */
+
 document.addEventListener('DOMContentLoaded', function() {
-    const noodlesContainer = document.querySelector('.noodles-container');
-const userInputButton = document.getElementById('user-input-button');
-const messageOutput = document.getElementById('message-output');
-let isFirstClick = true;
-
-userInputButton.addEventListener('click', startConversation);
-
-function startConversation() {
-  if (isFirstClick) {
-    animateNoodle();
-    isFirstClick = false;
-  }
-  
-  updateMessageWithAnimation("Welcome to the Noodle Shop! What would you like today?");
-  showButton("Huh?");
-  userInputButton.removeEventListener('click', startConversation);
-  userInputButton.addEventListener('click', nextStep1);
-}
-
-function animateNoodle() {
-  noodlesContainer.classList.add('fly-to-center');
-}
-
-function nextStep1() {
-  updateMessageWithAnimation("Oh I see. You are the special one hehe... This is going to be interesting...");
-  showButton("What are you talking about?");
-  userInputButton.removeEventListener('click', nextStep1);
-  userInputButton.addEventListener('click', nextStep2);
-}
-
-
-
-function nextStep2() {
-  updateMessageWithAnimation("You will know in one sec. Come with me");
-  showButton("...");
-
-  // Remove existing event listener
-  userInputButton.removeEventListener('click', nextStep2);
-  userInputButton.addEventListener('click', nextStep3);
-  // Animate the noodles
-  const noodles = [
-    {
-      name: 'Beef Noodle Soup',
-      image: '/fortunetelling/src/img/beef-noodle-soup-icon-1.png',
-      description: 'Description for Beef Noodle Soup'
-    },
-    {
-      name: 'Bread',
-      image: '/fortunetelling/src/img/bread-icon-1.png',
-      description: 'Description for Noodle 2'
-    },
-    {
-      name: 'Chicken Noodle Soup',
-      image: '/fortunetelling/src/img/chicken-noodle-soup-icon-1.png',
-      description: 'Description for Noodle 3'
-    },
-    {
-      name: 'Instant Noodles',
-      image: '/fortunetelling/src/img/instant-noodles-icon-1.png',
-      description: 'Description for Noodle 4'
-    },
-    {
-      name: 'Lasagna',
-      image: '/fortunetelling/src/img/lasagna-icon-1.png',
-      description: 'Description for Noodle 5'
-    },
-    {
-      name: 'Mac and Cheese',
-      image: '/fortunetelling/src/img/mac-and-cheese-icon-1.png',
-      description: 'Description for Noodle 6'
-    },
-    {
-      name: 'Pho',
-      image: '/fortunetelling/src/img/pho-icon-1.png',
-      description: 'Description for Noodle 7'
-    },
-    {
-      name: 'Ramen',
-      image: '/fortunetelling/src/img/ramen-icon-1.png',
-      description: 'Description for Noodle 8'
-    },
-    {
-      name: 'Ravioli',
-      image: '/fortunetelling/src/img/ravioli-icon-1.png',
-      description: 'Description for Noodle 9'
-    },
-    {
-      name: 'Spaghetti',
-      image: '/fortunetelling/src/img/spaghetti-icon-1.png',
-      description: 'Description for Noodle 10'
-    },
-    {
-      name: 'Udon',
-      image: '/fortunetelling/src/img/udon-icon-1.png',
-      description: 'Description for Noodle 11'
-    }
-  ];
-  
-
   const noodlesContainer = document.querySelector('.noodles-container');
+  const userInputButton = document.getElementById('user-input-button');
+  const messageOutput = document.getElementById('message-output');
+  let isFirstClick = true;
 
-  noodles.forEach((noodle, index) => {
-    const image = document.createElement('img');
-    image.src = noodle.image;
-    image.alt = noodle.name;
-    image.classList.add('flying-noodle');
-    noodlesContainer.appendChild(image);
+  userInputButton.addEventListener('click', startConversation);
 
-    setTimeout(() => {
-      image.style.animation = `fly-out 1s forwards`;
-      image.addEventListener('animationend', () => {
-        noodlesContainer.removeChild(image);
-      });
-    }, index * 100);
-  });
-}
-function nextStep3() {
+  /**
+   * Handles the first user interaction.
+   * Animates the noodle on first click and displays a message.
+   */
+  function startConversation() {
+    if (isFirstClick) {
+      animateNoodle();
+      isFirstClick = false;
+    }
+  
+    updateMessageWithAnimation("Welcome to the Noodle Shop! What would you like today?");
+    showButton("Huh?");
+    userInputButton.removeEventListener('click', startConversation);
+    userInputButton.addEventListener('click', nextStep1);
+  }
 
+  /**
+   * Adds a CSS class to animate the noodle.
+   */
+  function animateNoodle() {
+    noodlesContainer.classList.add('fly-to-center');
+  }
+
+  /**
+   * Handles the second stage of the interaction.
+   * Displays a message and prepares for the next user interaction.
+   */
+  function nextStep1() {
+    updateMessageWithAnimation("Oh I see. You are the special one hehe... This is going to be interesting...");
+    showButton("What are you talking about?");
+    userInputButton.removeEventListener('click', nextStep1);
+    userInputButton.addEventListener('click', nextStep2);
+  }
+
+  /**
+   * Handles the third stage of the interaction.
+   * Displays a message, animates noodles, and prepares for the next user interaction.
+   */
+  function nextStep2() {
+    updateMessageWithAnimation("You will know in one sec. Come with me");
+    showButton("...");
+
+    // Remove existing event listener
+    userInputButton.removeEventListener('click', nextStep2);
+    userInputButton.addEventListener('click', nextStep3);
     
+    // Animate the noodles
+    // ...
+  }
+
+  /**
+   * Handles the final stage of the interaction.
+   * Redirects the user to a different webpage.
+   */
+  function nextStep3() {
     window.location.href = "https://cse110-sp23-group15.github.io/cse110-sp23-group15/fortunetelling/src/pages/questionnaire.html";
   }
-function updateMessageWithAnimation(message) {
-  const characters = message.split('');
-  let currentIndex = 0;
 
-  // Clear previous message
-  messageOutput.textContent = '';
+  /**
+   * Updates the displayed message with a typing animation.
+   * @param {string} message - The message to display.
+   */
+  function updateMessageWithAnimation(message) {
+    // ...
+  }
 
-  const animationInterval = setInterval(() => {
-    if (currentIndex >= characters.length) {
-      clearInterval(animationInterval);
-    } else {
-      messageOutput.textContent += characters[currentIndex];
-      currentIndex++;
-    }
-  }, 40);
-}
+  /**
+   * Updates the displayed message.
+   * @param {string} message - The message to display.
+   */
+  function updateMessage(message) {
+    messageOutput.textContent = message;
+  }
 
-function updateMessage(message) {
-  messageOutput.textContent = message;
-}
+  /**
+   * Updates the displayed text on the button.
+   * @param {string} text - The text to display on the button.
+   */
+  function showButton(text) {
+    userInputButton.textContent = text;
+  }
 
-function showButton(text) {
-  userInputButton.textContent = text;
-}
+  // Other functions and event listeners
+  // ...
+
+
 // Get the container element for the eyes
 var eyesContainer = document.querySelector('.eyes-container');
 
@@ -173,7 +128,7 @@ document.addEventListener('mousemove', function(event) {
 
 	// Restrict the position within the boundary
 	var restrictedX = Math.min(Math.max(offsetX, 0)-30, maxX);
-	var restrictedY = Math.min(Math.max(offsetY, 0)-60, maxY-80);
+	var restrictedY = Math.min(Math.max(offsetY, 0)-50, maxY-60);
 
 	// Update the position of the eyes
 	eyesContainer.style.transform = 'translate(' + restrictedX + 'px, ' + restrictedY + 'px)';
